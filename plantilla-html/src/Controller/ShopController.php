5dc45afca41e1cda5238af5240e9f5b676d63869
Alexloopez5 <lopeznavarroalex5@gmail.com>
2023-11-03 08:54:59 +0100

    Plantilla modificada con twig

---
+++ plantilla-html/src/Controller/ShopController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShopController extends AbstractController
+{
+    #[Route('/shop', name: 'shop')]
+    public function index(): Response
+    {
+        return $this->render('shop/index.html.twig', []);
+    }
+    
+    #[Route('/shop-single', name: 'shop-single')]
+    public function indexShopSingle(): Response
+    {
+        return $this->render('shop/shop-singleindex.html.twig', []);
+    }
+}
